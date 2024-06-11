234da980dbc0e18ba41b28c4665494885382dc2e
Akhi111 <acharekarakhil@gmail.com>
2024-06-11 20:45:35 +0530

    Show the comments of a post

--- api/routes/comment.rout.js
+++ api/routes/comment.rout.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import { createComment } from "../controllers/comment.controller.js";
+import { getPostComments } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
+router.get("/getPostComments/:postId", getPostComments); // dont need to call verifyToken because comments are for all users i.e publically
+
 
 export default router;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,6 +5,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 import { deleteUser } from "../controllers/user.controller.js";
 import { signout } from "../controllers/user.controller.js";
 import { getUsers } from "../controllers/user.controller.js";
+import { getUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.put("/update/:userId", verifyToken, updateUser); //We can also use post r
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;

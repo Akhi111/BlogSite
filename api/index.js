2322637d5095a135a0b61cd431ed01030ffe29b5
Akhi111 <acharekarakhil@gmail.com>
2024-06-11 14:34:44 +0530

    create scrolltotop and add comment section to the post page and fix error in comment model schema

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.rout.js";
-import router from "./routes/post.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.rout.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -28,7 +29,8 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/post", router);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 //Middleware for Error Handling
 app.use((err, req, res, next) => {

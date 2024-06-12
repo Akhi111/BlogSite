70dea7b67b0b2716544ca202332342910d7a4674
Akhi111 <acharekarakhil@gmail.com>
2024-06-12 13:53:44 +0530

    added like functionality to the comment component

--- api/routes/comment.rout.js
+++ api/routes/comment.rout.js
@@ -2,11 +2,12 @@ import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import { createComment } from "../controllers/comment.controller.js";
 import { getPostComments } from "../controllers/comment.controller.js";
+import { likeComment } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments); // dont need to call verifyToken because comments are for all users i.e publically
-
+router.put("/likeComment/:commentId", verifyToken, likeComment);
 
 export default router;

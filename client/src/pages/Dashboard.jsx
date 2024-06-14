75cc9c4898881074b92d4953f61ba81876ffb727
Akhi111 <acharekarakhil@gmail.com>
2024-06-14 22:40:23 +0530

    Added dashboard overview to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -32,6 +33,8 @@ const Dashboard = () => {
       {tab === "users" && <DashUsers />}
       {/* comments */}
       {tab === "comments" && <DashComments />}
+      {/* dashboard component */}
+      {tab === "dash" && <DashboardComp />}
     </div>
   );
 };

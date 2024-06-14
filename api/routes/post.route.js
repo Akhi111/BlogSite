a18f940908c6c9efb2018f4e144ed1ca458377c1
Akhi111 <acharekarakhil@gmail.com>
2024-06-14 15:35:05 +0530

    added recent article section to the post page and created PostCard component

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -10,7 +10,7 @@ import {
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
-router.get("/getposts", getposts);
+router.get("/getposts", getposts); //we uses this api route to show recent posts below comments section. 
 router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,14 @@
 const flowbite = require("flowbite-react/tailwind");
 
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin(),require('tailwind-scrollbar'),],
+  plugins: [
+    flowbite.plugin(),
+    require("tailwind-scrollbar"),
+    
+  ],
 };
